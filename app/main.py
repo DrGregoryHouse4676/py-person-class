94f72cf4c1de46a844ff60d66b69e1ad6b4b3fd7
Lev <lev01111989@gmail.com>
2025-07-01 10:20:34 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -7,23 +7,30 @@ class Person:
         Person.people[name] = self
 
 
-def create_person_list(people_list: list) -> list:
+def create_person_list(people_list: dict) -> Person:
     Person.people.clear()
-    instance = [Person(name=person_data["name"],
-                       age=person_data["age"])
-                for person_data in people_list]
+
+    instances = [
+        Person(
+            name=person_data["name"],
+            age=person_data["age"]
+        )
+        for person_data in people_list
+    ]
 
     for person_data in people_list:
         person = Person.people[person_data["name"]]
 
-        if "wife" in person_data and person_data["wife"] is not None:
-            wife_person = Person.people[person_data["wife"]]
+        wife_name = person_data.get("wife")
+        if wife_name:
+            wife_person = Person.people[wife_name]
             person.wife = wife_person
             wife_person.husband = person
 
-        if "husband" in person_data and person_data["husband"] is not None:
-            husband_person = Person.people[person_data["husband"]]
+        husband_name = person_data.get("husband")
+        if husband_name:
+            husband_person = Person.people[husband_name]
             person.husband = husband_person
             husband_person.wife = person
 
-    return instance
+    return instances
