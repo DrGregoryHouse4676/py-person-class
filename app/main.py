800156628cd0e4a431eb9ef8df0a32e86308eddf
Lev <lev01111989@gmail.com>
2025-07-10 13:28:04 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -6,16 +6,25 @@ class Person:
         self.age = age
         Person.people[name] = self
 
+    def __repr__(self) -> None:
+        return f"Person(name=\"{self.name}\", age={self.age})"
+
 
 def create_person_list(people: list) -> list:
+    Person.people.clear()
+
     for person in people:
         Person(person["name"], person["age"])
 
     for person in people:
         current_person = Person.people[person["name"]]
-        if "wife" in person and person["wife"] is not None:
-            current_person.wife = Person.people[person["wife"]]
-        if "husband" in person and person["husband"] is not None:
-            current_person.husband = Person.people[person["husband"]]
+
+        wife_name = person.get("wife")
+        if wife_name and wife_name in Person.people:
+            current_person.wife = Person.people[wife_name]
+
+        husband_name = person.get("husband")
+        if husband_name and husband_name in Person.people:
+            current_person.husband = Person.people[husband_name]
 
     return [Person.people[person["name"]] for person in people]
